d9df3aa6960ffce64622eadc62cc8220b7822801
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-09-27 15:37:21 -0400

    Update Assignment2.java
    Step 1, import the scanner library and start with the basic user input

--- Assignment2.java
+++ Assignment2.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 public class Assignment2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        int numero = scanner.nextInt();
+
+        
     }
 }
