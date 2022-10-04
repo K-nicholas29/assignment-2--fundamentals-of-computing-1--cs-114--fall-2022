fab2103af4706aceff3769a7ee770adc413585cd
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-10-04 19:38:42 -0400

    Update Assignment2.java
    Top half of the diamond?

--- Assignment2.java
+++ Assignment2.java
@@ -13,13 +13,20 @@ public class Assignment2 {
         //User inputs whatever symbol they want to be used for the diamond
         System.out.println();
         //Creating a space between the symbol and the beginning of the diamond
+        
         for(int i = 1; i<=numero; i++) {
             for(int j = 1; j<=i; j++) {
                 System.out.print(cymbal); 
             }
+            for(int k = 1; k<i; k++){
+                System.out.print(cymbal);
+            }
+            for(int l = 1; l<=i; l++){
+                System.out.println(cymbal);
+            }
             System.out.println();
         }
-        //This code is for the first right triangle that makes up a diamond
+        //This code is for the top half of the diamond
 
         scanner.close();
     }
