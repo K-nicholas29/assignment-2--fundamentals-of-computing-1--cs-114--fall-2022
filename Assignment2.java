2ac0cf5cf780cdca313e8fc9f62bd54dd589edf8
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-10-04 19:04:21 -0400

    Update Assignment2.java
    Trying to see if the initial code to make a right triangle works

--- Assignment2.java
+++ Assignment2.java
@@ -4,13 +4,21 @@ public class Assignment2 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter a number: ");
-        int numero = scanner.nextInt();
+        // System.out.println("Please enter a number: ");
+        // int numero = scanner.nextInt();
         //User inputs a number to be used for the diamond
 
-        System.out.println("Enter a symbol to use: ");
-        String cymbal = scanner.next();
+        // System.out.println("Enter a symbol to use: ");
+        // String cymbal = scanner.next();
         //User inputs whatever symbol they want to be used for the diamond
+        
+        int chicken = 4;
+        for(int i = 1; i<=chicken; i++) {
+            for(int j = 1; j<=i; j++) {
+                System.out.println(cymbal); 
+            }
+            System.out.println();
+        }
 
         scanner.close();
     }
