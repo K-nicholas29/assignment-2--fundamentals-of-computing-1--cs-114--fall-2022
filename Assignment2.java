e68e2f6c0d761c3b6589481b580f710f17790abf
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-10-04 19:47:27 -0400

    Update Assignment2.java
    Changed first for loop to numero as well

--- Assignment2.java
+++ Assignment2.java
@@ -15,10 +15,10 @@ public class Assignment2 {
         //Creating a space between the symbol and the beginning of the diamond
         
         for(int i = 1; i<=numero; i++) {
-            for(int j = 1; j<=i; j++) {
+            for(int j = 1; j<=numero; j++) {
                 System.out.print(" "); 
             }
-            for(int j = 1; j<i; j++) {
+            for(int j = 1; j< i; j++) {
                 System.out.print(cymbal);
             }
             for(int j = 1; j<=i; j++) {
