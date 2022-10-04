7c7a8311eead5a6603671b7737283e6f9c2f5e19
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-10-04 19:58:26 -0400

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -15,7 +15,7 @@ public class Assignment2 {
         //Creating a space between the symbol and the beginning of the diamond
         
         for(int i = 1; i<=numero; i++) {
-            for(int j = 1; j<=numero; j++) {
+            for(int j = i; j<=numero; j++) {
                 System.out.print(" "); 
             }
             for(int j = 1; j< i; j++) {
