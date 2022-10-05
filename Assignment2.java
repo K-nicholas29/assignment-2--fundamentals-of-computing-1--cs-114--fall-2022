f101ac241dde0bebc7e8426d89b2258b1367bc27
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-10-04 20:44:39 -0400

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -18,10 +18,10 @@ public class Assignment2 {
             for(int j = i; j<=Userinputnumero; j++) {
                 System.out.print(" "); 
             }
-            for(int j = 1; j<=i; j++) {
+            for(int j = 0; j<=i; j++) {
                 System.out.print(cymbal);
             }
-            for(int j = 1; j<=i; j++) {
+            for(int j = 0; j<=i; j++) {
                 System.out.print(cymbal);
             }
             System.out.println();
