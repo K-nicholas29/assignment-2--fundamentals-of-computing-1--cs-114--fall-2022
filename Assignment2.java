f0e454bedf795fda4848fa8dfbb49ba52aa88be4
K-nicholas29 <111987510+K-nicholas29@users.noreply.github.com>
2022-10-04 20:37:57 -0400

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -27,7 +27,7 @@ public class Assignment2 {
             System.out.println();
         }
 
-        for(int k = 0; k<=Userinputnumero; k++) {
+        for(int k = Userinputnumero; k<=Userinputnumero; k++) {
             for(int j = 1; j<=k; j++) {
                 System.out.print(" ");
             }
